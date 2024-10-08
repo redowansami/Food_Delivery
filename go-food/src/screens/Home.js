988238d90f248c38f9ead88e73f344edbb4df640
Redowan Sami <bsse1409@iit.du.ac.bd>
2024-10-09 00:49:52 +0600

    updated react-app

---
+++ go-food/src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import Body from '../components/Body'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
