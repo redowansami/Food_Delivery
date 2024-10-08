c14a1fe3668316b60c4903a0feda216d2ef6b50d
Redowan Sami <bsse1409@iit.du.ac.bd>
2024-10-09 03:40:43 +0600

    added footer

--- go-food/src/components/Body.js
+++ go-food/src/components/Body.js
@@ -8,7 +8,24 @@ export default function Body() {
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">This is a card.</p>
-                        
+                        <div className="container w-100">
+                            <select className="m-2 h-100 bg-success rounded">
+                                {Array.from(Array(6), (e,i)=>{
+                                    return (
+                                        <option key={i+1} value={i+1}>{i+1}</option>
+                                    )
+                                })}
+                            </select>
+
+                            <select className="m-2 h-100 bg-success rounded">
+                                <option value="half">Half</option>
+                                <option value="full">Full</option>
+                            </select>
+
+                            <div className="d-inline h-100 fs-5">
+                                Total Price
+                            </div>
+                        </div>
                     </div>
             </div>
         
--- go-food/src/components/Footer.js
+++ go-food/src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
     <div>
-      <h1>Footer</h1>
+      <footer className="py-3 my-4">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <li className="nav-item"><Link href="/" className="nav-link px-2 text-muted">Home</Link></li>
+          <li className="nav-item"><Link href="/" className="nav-link px-2 text-muted">Features</Link></li>
+          <li className="nav-item"><Link href="/" className="nav-link px-2 text-muted">Pricing</Link></li>
+          <li className="nav-item"><Link href="/" className="nav-link px-2 text-muted">FAQs</Link></li>
+          <li className="nav-item"><Link href="/" className="nav-link px-2 text-muted">About</Link></li>
+        </ul>
+        <p className="text-center text-muted">© 2024 GoFood, Inc</p>
+      </footer>
     </div>
   )
 }
